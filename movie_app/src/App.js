401ce1d403dcfb8632ccb0b70c1317031a9da7ec
devcation <dynd0863@gmail.com>
2018-11-01 23:56:21 +0900

    loading state

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -8,26 +8,8 @@ import Movie from './Movie';
 class App extends Component {
 
   state = {
-    greeting: `hello`,
-
-    movies : [
-      {
-        title :"matrix",
-        poster : "http://ticketimage.interpark.com/Movie/still_image/V16/V1601447p_s01.gif"
-      },
-      {
-        title : "Full Metal Jacket",
-        poster : "https://www.imagetoday.co.kr/images/event/event_201810_pop.jpg"
-      },
-      {
-        title : "Old Boy",
-        poster : "http://img2.sbs.co.kr/img/sbs_cms/PG/2015/09/04/PG39131527_w1280_h720.jpg"
-      },
-      {
-        title : "Star Wars",
-        poster : "http://ticketimage.interpark.com/Movie/still_image/V16/V1601447p_s01.gif"
-      }
-    ]
+  
+    
   }
 
   componentDidMount(){
@@ -41,14 +23,34 @@ class App extends Component {
           movies: [
             
             {
+              title :"matrix",
+              poster : "http://ticketimage.interpark.com/Movie/still_image/V16/V1601447p_s01.gif"
+            },
+            {
+              title : "Full Metal Jacket",
+              poster : "https://www.imagetoday.co.kr/images/event/event_201810_pop.jpg"
+            },
+            {
               title : "Old Boy",
               poster : "http://img2.sbs.co.kr/img/sbs_cms/PG/2015/09/04/PG39131527_w1280_h720.jpg"
             },
-            ...this.state.movies,
+            {
+              title : "Star Wars",
+              poster : "http://ticketimage.interpark.com/Movie/still_image/V16/V1601447p_s01.gif"
+            }
           ]
         })
     }, 5000)
   }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map( (movie ,index)=> {
+            
+      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    })
+    return movies
+  }
+
 
   render() {
     console.log("render")
@@ -56,10 +58,7 @@ class App extends Component {
     return (
 
       <div className="App">
-        {this.state.movies.map( (movie ,index)=> {
-          
-          return <Movie title={movie.title} poster={movie.poster} key={index}/>
-        })}
+        {this.state.movies ? this._renderMovies() : 'loading' }
 
       </div>
     );
